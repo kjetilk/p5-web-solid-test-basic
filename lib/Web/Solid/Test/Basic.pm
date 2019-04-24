fd38ca495d2dc6833a1b80f8ca9d219687d22b7d
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2019-04-24 12:05:26 +0200

    Initial stub

---
+++ lib/Web/Solid/Test/Basic.pm
@@ -0,0 +1,51 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package Web::Solid::Test::Basic;
+
+our $AUTHORITY = 'cpan:KJETILK';
+our $VERSION   = '0.001';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Web::Solid::Test::Basic - Basic Solid Tests
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<https://github.com/kjetilk/p5-web-solid-test-basic/issues>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo E<lt>kjetilk@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is Copyright (c) 2019 by Inrupt Inc.
+
+This is free software, licensed under:
+
+  The MIT (X11) License
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
