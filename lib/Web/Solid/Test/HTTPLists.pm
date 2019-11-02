c8c9ca2b704471a15288fb184beaa41cfd05f772
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2019-11-02 01:19:25 +0100

    Bump version; Update changelog

--- lib/Web/Solid/Test/HTTPLists.pm
+++ lib/Web/Solid/Test/HTTPLists.pm
@@ -11,7 +11,7 @@ use Test::RDF;
 use Data::Dumper;
 
 our $AUTHORITY = 'cpan:KJETILK';
-our $VERSION   = '0.004';
+our $VERSION   = '0.005_01';
 
 my $bearer_predicate = 'http://example.org/httplist/param#bearer'; # TODO: Define proper URI
 
